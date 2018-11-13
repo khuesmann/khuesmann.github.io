aab7c3ee0431c7b8893329faddaf4f03b63a574c
karim <k@h.com>
2018-11-13 09:52:42 +0100

    inital

---
+++ precache-manifest.b92e8934399c39f72ad91b9118293d7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "0f42e090ba9e4acbbd61",
+    "url": "js/app.46523950.js"
+  },
+  {
+    "revision": "cdf7ccacef2f8a3b5d1d",
+    "url": "js/about.11fcb2df.js"
+  },
+  {
+    "revision": "9d3a0fdf189a648fb9a385b7687dbc8f",
+    "url": "index.html"
+  },
+  {
+    "revision": "26bc76b5a44be7ab7d1ece7bf29d06de",
+    "url": "img/tower.26bc76b5.jpg"
+  },
+  {
+    "revision": "0f42e090ba9e4acbbd61",
+    "url": "css/app.21f4647c.css"
+  },
+  {
+    "revision": "16803c376a3541ad5a7474b177ddd604",
+    "url": "31abf4dd256583ccb04c.worker.js"
+  }
+];
